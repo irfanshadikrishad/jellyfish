9cd1b931c1a1e6e24953a4c91ec0d1cd2447312a
Irfan Shadik Rishad <77362427+irfanshadikrishad@users.noreply.github.com>
2024-06-27 09:00:40 +0600

    feature: insertbyrange and deletebyid (#1)

--- index.ts
+++ index.ts
@@ -3,19 +3,3 @@ import database from "./database/database";
 import chalk from "chalk";
 
 database();
-
-Jellyfish.singleInsertById("131930")
-  .then((data) => {
-    if (data && data._id) {
-      console.log(
-        chalk.green(
-          `[singleInsertById] ${data.anilistId} ${data._id} ${
-            data.title.english && data.title.english
-          }`
-        )
-      );
-    }
-  })
-  .catch((error) => {
-    console.log(chalk.magenta(error));
-  });
