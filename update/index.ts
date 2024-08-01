afd55b36e805b7896f53e3813744a84736c28e59
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2024-08-02 04:20:19 +0600

    fix: updater automation to be express server

--- update/index.ts
+++ update/index.ts
@@ -2,25 +2,46 @@ import { Jellyfish } from "../core/jellyfish";
 import database from "../database/database";
 import { colorize_info, colorize_mark2 } from "../utils/colorize";
 import { sendMail } from "../utils/workers";
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3001;
 
 colorize_info(`running.`);
 setInterval(async () => {
   try {
-    colorize_info(`Initiating ongoing updates...`);
-    await database();
-    await Jellyfish.updateAllOngoing()
-      .then((count) => {
-        if (count) {
-          colorize_mark2(`[update] +${count} episodes.`);
-          sendMail(`${count}`);
-        } else {
-          sendMail("0");
-        }
-      })
-      .catch((error) => {
-        sendMail("0", String(error));
-      });
+    await update();
   } catch (error) {
     sendMail("0", String(error));
   }
 }, 10800000); // Runs every 3hrs
+
+app.get("/", (req, res) => {
+  res.status(200).json({ status: 200 });
+});
+
+app.get("/update", async (req, res) => {
+  await update();
+  res.status(200).json({ status: 200 });
+});
+
+app.listen(port, () => {
+  console.log(`[server] ${port}`);
+});
+
+async function update() {
+  colorize_info(`Initiating ongoing updates...`);
+  await database();
+  await Jellyfish.updateAllOngoing()
+    .then((count) => {
+      if (count) {
+        colorize_mark2(`[update] +${count} episodes.`);
+        sendMail(`${count}`);
+      } else {
+        sendMail("0");
+      }
+    })
+    .catch((error) => {
+      sendMail("0", String(error));
+    });
+}
