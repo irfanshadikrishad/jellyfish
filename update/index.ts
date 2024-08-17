36aa74e192bb515ab16b83dd4ed1140368f26a95
Irfan Shadik Rishad <77362427+irfanshadikrishad@users.noreply.github.com>
2024-08-17 06:00:15 +0600

    feat: update all dubs (#5)

--- update/index.ts
+++ update/index.ts
@@ -1,6 +1,10 @@
 import { Jellyfish } from "../core/jellyfish";
 import database from "../database/database";
-import { colorize_info, colorize_mark2 } from "../utils/colorize";
+import {
+  colorize_error,
+  colorize_info,
+  colorize_mark2,
+} from "../utils/colorize";
 import { sendMail } from "../utils/workers";
 import express from "express";
 
@@ -10,10 +14,13 @@ const port = process.env.PORT || 3001;
 app.get("/", (req, res) => {
   res.status(200).json({ status: 200 });
 });
-
 app.get("/update", async (req, res) => {
   await update();
   res.status(200).json({ status: 200 });
+});
+app.get("/update-all-dubs", async (req, res) => {
+  await update_dubs();
+  res.status(200).json({ status: 200, message: `updated` });
 });
 
 app.listen(port, () => {
@@ -39,6 +46,17 @@ async function update() {
           colorize_info(`environment variable not found.`);
         }
       }
+    })
+    .catch((error) => {
+      sendMail("0", String(error));
+    });
+}
+
+async function update_dubs() {
+  await database();
+  await Jellyfish.updateAllDubs()
+    .then((count) => {
+      sendMail(String(count?.updated));
     })
     .catch((error) => {
       sendMail("0", String(error));
